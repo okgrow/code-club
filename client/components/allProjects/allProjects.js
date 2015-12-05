fad1d65e06c5d82472a6a332ea3c4463d9784a7d
Sergiy Dybskiy <me@416serg.com>
2015-12-05 12:11:42 -0500

    had no idea #each had an else

--- client/components/allProjects/allProjects.js
+++ client/components/allProjects/allProjects.js
@@ -8,8 +8,5 @@ Template.allProjects.onRendered(function() {
 Template.allProjects.helpers({
   allProjects: function () {
     return Projects.find({meetupId: this._id}, {sort: {name: 1}});
-  },
-  atLeastOne: function() {
-  	return Projects.find({meetupId: this._id}, {sort: {name: 1}}).fetch().length > 0;
   }
 });
