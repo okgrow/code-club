ffd499a95d68551f02a0d143a069759323c8ea24
Paul Dowman <paul@pauldowman.com>
2016-01-06 19:50:19 -0500

    Fix timezone bug
    Meteor hosting local time is UTC. Hard-code UTC-5.

--- server/meetups.js
+++ server/meetups.js
@@ -1,8 +1,8 @@
 currentMeetupCursor = function () {
-  // Time 00:00 today
-  var today = new Date().setHours(0);
+  // Time 00:00 today in UTC-5 time zone
+  var today = moment().utcOffset(-300).startOf('day').toDate();
 
-  return Meetups.find({time: {$gt: today}}, {limit: 1});
+  return Meetups.find({time: {$gt: today.getTime()}}, {limit: 1});
 };
 
 Meteor.publish("currentMeetup", function() {
