4c55a1ce73e7cffce21ca48907893f5bbf139b5f
Paul Dowman <paul@pauldowman.com>
2016-01-06 19:58:02 -0500

    Handle case with no meetups in db

--- server/projects.js
+++ server/projects.js
@@ -1,4 +1,9 @@
 Meteor.publish("projects", function () {
+  if (currentMeetupCursor.count() === 0) {
+    console.error("No meetups found!");
+    this.ready();
+    return;
+  }
   var currentMeetup = currentMeetupCursor().fetch()[0];
   return Projects.find({meetupId: currentMeetup._id});
 });
