76894a19035c56cf96cd5e7d6bb4fed900ad06b8
Mark Gituma <gitumarkk@gmail.com>
2016-06-03 12:20:05 -0400

    added the project page

---
+++ imports/ui/pages/projects/create.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default ProjectCreatePage = React.createClass({
+    displayName: 'ProjectCreatePage',
+    render() {
+        return (
+            <div>ProjectCreatePage</div>
+        );
+    }
+});
