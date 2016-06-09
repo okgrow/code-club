9e237fa5ff59a0f66e6f65968420b65c954ed9cc
Mark Gituma <gitumarkk@gmail.com>
2016-06-09 17:45:10 -0400

    added ability to edit the project

--- imports/ui/pages/projects/create.jsx
+++ imports/ui/pages/projects/create.jsx
@@ -15,25 +15,50 @@ import { Projects } from '/imports/api/projects/collections.js';
 export default ProjectCreatePage = React.createClass({
     displayName: 'ProjectCreatePage',
 
+    propTypes: {
+        currentProject: React.React.PropTypes.object,
+        currentMeetup: React.React.PropTypes.object,
+        projectId: React.PropTypes.string
+    },
+
     submitForm(ev) {
         ev.preventDefault();
         // If user is not authenticated or the current meetup as not been set
         if (!this.props.currentUser || !this.props.currentMeetup) return;
+        const name = ev.target.name.value;
+        const gitHubUrl = ev.target.gitHubUrl.value;
+        const description = ev.target.description.value;
 
-        Projects.insert({
-            name: ev.target.name.value,
-            gitHubUrl: ev.target.gitHubUrl.value,
-            ownerName: this.props.currentUser.profile.name,
-            ownerId: this.props.currentUser._id,
-            meetupId: this.props.currentMeetup._id,
-            description: ev.target.description.value
-        });
+        // If current project do an update
+        if (this.props.currentProject &&
+            this.props.currentProject.ownerId === this.props.currentUser._id) {
+            // Prevent non project owners from updating project
+            Projects.update(this.props.currentProject._id, {
+                $set: {
+                    name,
+                    gitHubUrl,
+                    description
+                }
+            });
+
+        } else { // If not current project is set do an insert
+            Projects.insert({
+                name,
+                gitHubUrl,
+                description,
+                ownerName: this.props.currentUser.profile.name,
+                ownerId: this.props.currentUser._id,
+                meetupId: this.props.currentMeetup._id,
+                createdAt: Date()
+            });
+        }
 
         FlowRouter.go('/');
     },
 
     renderForm() {
         if (this.props.currentUser) {
+            const { name, gitHubUrl, description } = this.props.currentProject || {};
             return (
                 <form id="new-project" onSubmit={this.submitForm}>
                     <div className="form-group">
@@ -46,7 +71,9 @@ export default ProjectCreatePage = React.createClass({
                                 name="name"
                                 className="form-control"
                                 placeholder="Name"
-                                required />
+                                defaultValue={ name }
+                                required
+                            />
                         </div>
                     </div>
 
@@ -58,7 +85,9 @@ export default ProjectCreatePage = React.createClass({
                             <input type="url"
                                 name="gitHubUrl"
                                 className="form-control"
-                                placeholder="GitHub Link" />
+                                placeholder="GitHub Link"
+                                defaultValue={ gitHubUrl }
+                            />
                         </div>
                     </div>
 
@@ -66,12 +95,15 @@ export default ProjectCreatePage = React.createClass({
                         <textarea name="description"
                             className="form-control"
                             rows="3"
-                            placeholder="Description...">
+                            placeholder="Description..."
+                            defaultValue={ description } >
                         </textarea>
                     </div>
 
                     <div className="form-group">
-                        <input type="submit" value="Create Project" className="btn btn-primary" />
+                        <input type="submit"
+                            value={ this.props.currentProject ? "Edit Project" : "Create Project" }
+                            className="btn btn-primary" />
                     </div>
                 </form>
             );
@@ -94,7 +126,7 @@ export default ProjectCreatePage = React.createClass({
         return (
             <div className="panel panel-default">
                 <div className="panel-heading">
-                    <h3 className="panel-title">Add Project</h3>
+                    <h3 className="panel-title">{ this.props.currentProject ? "Edit" : "Add" } Project</h3>
                 </div>
 
                 <div className="panel-body">
@@ -105,18 +137,30 @@ export default ProjectCreatePage = React.createClass({
     }
 });
 
-export const ProjectCreateContainer = createContainer(() => {
+export const ProjectCreateContainer = createContainer((params) => {
     const handler = Meteor.subscribe("currentMeetup");
     const data = {
         isLoading: !handler.ready(),
         currentUser: Meteor.user()
     };
 
+    // If projectId is provided assume it's an edit
+    if (params.projectId) {
+        const projectHandler = Meteor.subscribe("projects.currentProject", params.projectId);
+        data.isLoading = data.isLoading || !projectHandler.ready();
+    }
+
     if (!data.isLoading) {
         const today = new Date().setHours(0);
         data.currentMeetup = Meetups.findOne({
             time: { $gt: today }
         }, { limit: 1 });
+
+        if (params.projectId) {
+            data.currentProject = Projects.findOne({
+                _id: params.projectId
+            });
+        };
     }
     return data;
 }, ProjectCreatePage);
