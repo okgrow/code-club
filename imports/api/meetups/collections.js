6c31c5a3a98ac1f309f0130929eba5d86276ed09
Paul Dowman <paul@pauldowman.com>
2016-07-07 10:42:51 -0400

    Allow meetups collection to be global for debugging

--- imports/api/meetups/collections.js
+++ imports/api/meetups/collections.js
@@ -1,6 +1,15 @@
 // Meteor imports
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
 // Meetup.com groups and their events
 export const Meetups = new Mongo.Collection('meetups');
 // Default policy denies all updates
+
+// Useful for debugging
+if (Meteor.isClient) {
+  window.Meetups = Meetups;
+}
+if (Meteor.isServer) {
+  global.Meetups = Meetups;
+}
